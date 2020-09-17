8cd20f7507dffc6fcf99b22209a46d0399cde38e
BhuvaneshKumar <32215672+bhuvanesh42@users.noreply.github.com>
2020-09-17 12:22:26 +0530

    Ar Core

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ArCoreController arCoreController;
+
+  onarcoreview(ArCoreController _arcoreController) {
+    arCoreController = _arcoreController;
+    sphere(arCoreController);
+  }
+
+  sphere(ArCoreController spherecontroller) {
+    final material = ArCoreMaterial(color: Colors.black);
+    final sphere = ArCoreSphere(materials: [material], radius: 1);
+    final node = ArCoreNode(shape: sphere, position: vector.Vector3(0, 0, 1));
+    spherecontroller.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: ArCoreView(onArCoreViewCreated: onarcoreview),
+      ),
+    );
+  }
+}
